2e2b01da12a245383c371b12248826214d95f436
Rohith.P <rohith132007@gmail.com>
2026-06-08 17:07:13 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,10 @@
+My_list=['one', 'two', 'three' ]
+print(My_list)
+print(len(My_list))
+print(My_list[1])
+My_list.append('four')
+print(My_list)
+another_list=['Apple','Oraange']
+new_list=My_list+another_list
+print(new_list)
+print(new_list[::-1])
