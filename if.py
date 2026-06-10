f1dc646546a8b7386475b92853459a4385f8056e
Rohith.P <rohith132007@gmail.com>
2026-06-10 15:45:38 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,23 @@
+n = int(input("Enter a number: "))
+
+# if statement
+if n > 0:
+    print("Positive Number")
+
+# if-else statement
+if n % 2 == 0:
+    print("Even Number")
+else:
+    print("Odd Number")
+
+# for loop
+print("Numbers from 1 to 5 using for loop:")
+for i in range(1, 6):
+    print(i)
+
+# while loop
+print("Numbers from 1 to 5 using while loop:")
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
