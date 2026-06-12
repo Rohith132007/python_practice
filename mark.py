23b2ca1a2995252be90f8184da789da733a15785
Rohith.P <rohith132007@gmail.com>
2026-06-12 15:53:59 +0530

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,12 @@
+i = int(input('Enter the mark'));
+if(i>=90):
+    print('the student is first class');
+elif(i>=75):
+    print('the student is second class');
+elif(i>=65):
+    print('the student is third class');
+elif(i>=45):
+    print('the student is just pass');
+else:
+    print('the student is fail');
+    
