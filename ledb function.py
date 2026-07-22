718036ea906066ce4e5168a240f4957971eefb57
Rohith.P <rohith132007@gmail.com>
2026-07-22 19:28:14 +0530

    Add files via upload

---
+++ ledb function.py
@@ -0,0 +1,15 @@
+# global variable
+x =  'ANIME'
+def func():
+    #enclosed variable
+   # x = 'ONE PIECE'
+    def name():
+    #local variable
+        print(f'I LOVE WATCHING  {x}')
+    
+    name()
+
+func()
+    
+
+    
