bb4eaf7b40e67e4a7f8f4526aaee10424c07a45b
Rohith.P <rohith132007@gmail.com>
2026-07-25 19:48:51 +0530

    Add files via upload

---
+++ code3.py
@@ -0,0 +1,14 @@
+print("Multiplication Table")
+num = int(input("Enter a number: "))
+
+print("Table of", num)
+
+for i in range(1, 11):
+    result = num * i
+    print(num, "x", i, "=", result)
+
+print("Table completed")
+print("Thank you")
+print("Program finished")
+print("----------------")
+print("End")
