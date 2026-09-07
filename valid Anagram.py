2b292f8dfb26a7a31388e375c7cd6b6e463156f0
Rohith.P <rohith132007@gmail.com>
2026-09-07 18:56:45 +0530

    Add files via upload

---
+++ valid Anagram.py
@@ -0,0 +1,50 @@
+'''
+
+============================== Leet code Problem ==============================
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        counter = {}
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+
+        for char in t:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+
+        return True
+        
+=============================== Leet code Problem ===============================
+
+'''
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        counter = {}
+
+        for char in s:
+            counter[char] = counter.get(char, 0) + 1
+
+        for char in t:
+            if char not in counter or counter[char] == 0:
+                return False
+            counter[char] -= 1
+
+        return True
+
+
+solution = Solution()
+
+s = "anagram"
+t = "nagaram"
+
+result = solution.isAnagram(s, t)
+
+print(result)
