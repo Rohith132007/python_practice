4a4735a15711d9accc73887da3ca61d7aba6c8e3
Rohith.P <rohith132007@gmail.com>
2026-09-19 17:05:05 +0530

    Add files via upload

---
+++ number of 1 bits.py
@@ -0,0 +1,38 @@
+'''
+
+=================================== Leet Code Problem =============================
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        num = n
+        while num:
+            res += num & 1
+            num = num >> 1
+
+        return res
+
+================================== test cases =============================
+
+'''
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+
+        num = n
+
+        while num:
+            res += num & 1
+            num = num >> 1
+
+        return res
+
+
+n = 11
+
+solution = Solution()
+
+answer = solution.hammingWeight(n)
+
+print("Number of 1 bits:", answer)
