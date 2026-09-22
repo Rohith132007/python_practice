12392151b028317d8bba7aa02e994b0c5fec9a0b
Rohith.P <rohith132007@gmail.com>
2026-09-22 19:46:06 +0530

    Add files via upload

---
+++ power of two.py
@@ -0,0 +1,30 @@
+'''
+
+================================= Leet Code Problem =================================
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n = n // 2
+
+        return n == 1
+        
+================================= Test case =========================================
+
+'''
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n = n // 2
+
+        return n == 1
+    
+solution = Solution()
+
+print(solution.isPowerOfTwo(2))
+print(solution.isPowerOfTwo(16))
